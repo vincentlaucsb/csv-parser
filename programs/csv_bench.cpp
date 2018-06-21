0a63d112da8c16e42d9407a93c735cdf590978ca
vincentlaucsb <vincela9@gmail.com>
2018-06-21 15:11:40 -0700

    Added some example programs

---
+++ programs/csv_bench.cpp
@@ -0,0 +1,37 @@
+// Calculate benchmarks for CSV parser
+
+#include "csv_parser.h"
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    using namespace csv;
+
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [file]" << std::endl;
+        exit(1);
+    }
+
+    // Benchmark 1: File IO + Parsing
+    std::string filename = argv[1];
+    auto start = std::chrono::system_clock::now();
+    auto info = get_file_info(filename);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << "Parsing took (including disk IO): " << diff.count() << std::endl;
+
+    // Benchmark 2: Parsing Only
+    std::ifstream csv(filename);
+    std::stringstream buffer;
+    buffer << csv.rdbuf();
+
+    start = std::chrono::system_clock::now();
+    parse(buffer.str());
+    end = std::chrono::system_clock::now();
+    diff = end - start;
+
+    std::cout << "Parsing took: " << diff.count() << std::endl;
+
+    return 0;
+}
---
+++ programs/csv_info.cpp
@@ -0,0 +1,21 @@
+#include "csv_parser.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    using namespace csv;
+
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [file]" << std::endl;
+        exit(1);
+    }
+
+    std::string file = argv[1];
+    auto info = get_file_info(file);
+
+    std::cout << file << std::endl
+        << "Columns: " << helpers::format_row(info.col_names, ", ")
+        << "Dimensions: " << info.n_rows << " rows x " << info.n_cols << " columns" << std::endl
+        << "Delimiter: " << info.delim << std::endl;
+
+    return 0;
+}
