980e94895e0d787a94169681145f2aeaa6cb0c9d
Vincent La <vincela9@gmail.com>
2019-05-18 23:53:45 -0700

    Added CMake based benchmarking

--- programs/data_type_bench.cpp
+++ programs/data_type_bench.cpp
@@ -5,25 +5,11 @@
 #define NDEBUG
 #endif
 
-int main(int argc, char** argv) {
+long double get_max(std::string file, std::string column, bool use_std = false);
+
+long double get_max(std::string file, std::string column, bool use_std) {
     using namespace csv;
-
-    if (argc < 3) {
-        std::cout << "Usage: " << argv[0] << " [file] [column]" << std::endl;
-        exit(1);
-    }
-
-    std::string file = argv[1],
-        column = argv[2];
-
-    bool use_std = false;
-
-    if (argc == 4) {
-        use_std = true;
-    }
-
     long double max = -std::numeric_limits<long double>::infinity();
-
     CSVReader reader(file);
 
     for (auto& row : reader) {
@@ -47,6 +33,40 @@ int main(int argc, char** argv) {
         }
     }
 
+    return max;
+}
+
+int main(int argc, char** argv) {
+    using namespace csv;
+
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " [file] [column]" << std::endl;
+        exit(1);
+    }
+
+    std::string file = argv[1],
+        column = argv[2];
+
+    long double max = 0, std_avg = 0, csv_avg = 0;
+    const long double trials = 5;
+    
+
+    for (size_t i = 0; i < trials; i++) {
+        auto start = std::chrono::system_clock::now();
+        max = get_max(file, column, true);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        std_avg += diff.count() / trials;
+
+        start = std::chrono::system_clock::now();
+        max = get_max(file, column, false);
+        end = std::chrono::system_clock::now();
+        diff = end - start;
+        csv_avg += diff.count() / trials;
+    }
+
+    std::cout << "std::from_chars: " << std_avg << std::endl;
+    std::cout << "csv::data_type: " << csv_avg << std::endl;
     std::cout << "Maximum value: " << max << std::endl;
 
     return 0;
