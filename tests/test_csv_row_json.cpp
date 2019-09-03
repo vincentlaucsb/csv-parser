2d05fc71f2559f7916313447c938b49be70594ae
Vincent La <vincela9@gmail.com>
2019-09-02 22:33:29 -0700

    Update test_csv_row_json.cpp

--- tests/test_csv_row_json.cpp
+++ tests/test_csv_row_json.cpp
@@ -40,7 +40,14 @@ TEST_CASE("CSVRow to_json() Test - Mixed", "[csv_mixed_row_to_json]") {
         { "A", "B", "C", "D", "E" }                     // Column names
     );
 
-    REQUIRE(row.to_json() == "{\"A\":1234.3,\"B\":234,\"C\":\"ABCD\",\"D\":\"AB1\",\"E\":1337}");
+    SECTION("Full Row") {
+        REQUIRE(row.to_json() == "{\"A\":1234.3,\"B\":234,\"C\":\"ABCD\",\"D\":\"AB1\",\"E\":1337}");
+    }
+
+    SECTION("Subset") {
+        REQUIRE(row.to_json({ "B", "C" }) == "{\"B\":234,\"C\":\"ABCD\"}");
+        REQUIRE(row.to_json({ "B", "A" }) == "{\"B\":234,\"A\":1234.3}");
+    }
 }
 
 TEST_CASE("CSVRow to_json_array() Test() - Mixed", "[csv_mixed_row_to_json_array]") {
@@ -49,5 +56,12 @@ TEST_CASE("CSVRow to_json_array() Test() - Mixed", "[csv_mixed_row_to_json_array
         { "A", "B", "C", "D", "E" }                     // Column names
     );
 
-    REQUIRE(row.to_json_array() == "[1234.3,234,\"ABCD\",\"AB1\",1337]");
+    SECTION("Full Row") {
+        REQUIRE(row.to_json_array() == "[1234.3,234,\"ABCD\",\"AB1\",1337]");
+    }
+
+    SECTION("Subset") {
+        REQUIRE(row.to_json_array({ "B", "C" }) == "[234,\"ABCD\"]");
+        REQUIRE(row.to_json_array({ "B", "A" }) == "[234,1234.3]");
+    }
 }
