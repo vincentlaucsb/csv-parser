e738d60cf106717b54857ace36d3b3a8c10fb167
Ryan A. Pavlik <ryan.pavlik@gmail.com>
2020-03-11 18:24:11 -0500

    Put unmovable state into a unique_ptr-managed struct. (#78)
    Got warnings on GCC/Clang about explicitly-defaulted
    move constructors/assignments implicitly deleted because
    std::mutex and std::condition_variable are not movable.
    This puts them into a heap allocation whose ownership
    can be moved.

--- include/internal/csv_reader.cpp
+++ include/internal/csv_reader.cpp
@@ -223,7 +223,7 @@ namespace csv {
     CSV_INLINE CSVReader::CSVReader(CSVFormat format) :
         delimiter(format.get_delim()), quote_char(format.quote_char),
         header_row(format.header), strict(format.strict),
-        unicode_bom_scan(!format.unicode_detect) {
+        unicode_bom_scan(!format.unicode_detect), feed_state(new ThreadedReadingState)  {
         if (!format.col_names.empty()) {
             this->set_col_names(format.col_names);
         }
@@ -246,7 +246,7 @@ namespace csv {
      *  \snippet tests/test_read_csv.cpp CSVField Example
      *
      */
-    CSV_INLINE CSVReader::CSVReader(csv::string_view filename, CSVFormat format) {
+    CSV_INLINE CSVReader::CSVReader(csv::string_view filename, CSVFormat format) : feed_state(new ThreadedReadingState) {
         /** Guess delimiter and header row */
         if (format.guess_delim()) {
             auto guess_result = guess_format(filename, format.possible_delimiters);
@@ -479,13 +479,13 @@ namespace csv {
          *         thread pulls data from disk)
          */
         while (true) {
-            std::unique_lock<std::mutex> lock{ this->feed_lock }; // Get lock
-            this->feed_cond.wait(lock,                            // Wait
-                [this] { return !(this->feed_buffer.empty()); });
+            std::unique_lock<std::mutex> lock{ this->feed_state->feed_lock }; // Get lock
+            this->feed_state->feed_cond.wait(lock,                            // Wait
+                [this] { return !(this->feed_state->feed_buffer.empty()); });
 
             // Wake-up
-            auto in = std::move(this->feed_buffer.front());
-            this->feed_buffer.pop_front();
+            auto in = std::move(this->feed_state->feed_buffer.front());
+            this->feed_state->feed_buffer.pop_front();
 
             // Nullptr --> Die
             if (!in.first) break;
@@ -544,23 +544,23 @@ namespace csv {
 
             if (current_strlen >= 0.9 * BUFFER_UPPER_LIMIT) {
                 processed += (line_buffer - buffer.get());
-                std::unique_lock<std::mutex> lock{ this->feed_lock };
+                std::unique_lock<std::mutex> lock{ this->feed_state->feed_lock };
 
-                this->feed_buffer.push_back(std::make_pair<>(std::move(buffer), current_strlen));
+                this->feed_state->feed_buffer.push_back(std::make_pair<>(std::move(buffer), current_strlen));
 
                 buffer = std::unique_ptr<char[]>(new char[BUFFER_UPPER_LIMIT]); // New pointer
                 line_buffer = buffer.get();
                 line_buffer[0] = '\0';
 
-                this->feed_cond.notify_one();
+                this->feed_state->feed_cond.notify_one();
             }
         }
 
         // Feed remaining bits
-        std::unique_lock<std::mutex> lock{ this->feed_lock };
-        this->feed_buffer.push_back(std::make_pair<>(std::move(buffer), line_buffer - buffer.get()));
-        this->feed_buffer.push_back(std::make_pair<>(nullptr, 0)); // Termination signal
-        this->feed_cond.notify_one();
+        std::unique_lock<std::mutex> lock{ this->feed_state->feed_lock };
+        this->feed_state->feed_buffer.push_back(std::make_pair<>(std::move(buffer), line_buffer - buffer.get()));
+        this->feed_state->feed_buffer.push_back(std::make_pair<>(nullptr, 0)); // Termination signal
+        this->feed_state->feed_cond.notify_one();
         lock.unlock();
         worker.join();
 
