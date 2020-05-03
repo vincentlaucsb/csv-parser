4fdd51d19b485073f98632e4f298d14e46de3644
Kim Walisch (via Travis CI) <kim.walisch@gmail.com>
2020-05-03 07:14:49 +0000

    Deploy vincentlaucsb/csv-parser to github.com/vincentlaucsb/csv-parser.git:gh-pages

--- html/search/functions_10.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['variable_5fcolumns',['variable_columns',['../classcsv_1_1CSVFormat.html#a00d9f6ea821cb0570c3088cebf5e2f62',1,'csv::CSVFormat::variable_columns(VariableColumnPolicy policy=VariableColumnPolicy::IGNORE)'],['../classcsv_1_1CSVFormat.html#a3434d223edcc2753851e448b145fe944',1,'csv::CSVFormat::variable_columns(bool policy)']]]
-];
--- html/search/functions_e.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['set_5fcol_5fnames',['set_col_names',['../group__csv__internal.html#ga9946b7ff4e216e927c17c05aa02c2555',1,'csv::CSVReader']]],
-  ['size',['size',['../classcsv_1_1CSVRow.html#a6bb43e7a3bafdda37d0c4625360f0fb7',1,'csv::CSVRow::size()'],['../classcsv_1_1internals_1_1RawRowBuffer.html#a230b4593a060126d4911ad859d6a133c',1,'csv::internals::RawRowBuffer::size()']]],
-  ['splits_5fsize',['splits_size',['../classcsv_1_1internals_1_1RawRowBuffer.html#ad0f94ee70b1083f83aaf40212c82e123',1,'csv::internals::RawRowBuffer']]]
-];
