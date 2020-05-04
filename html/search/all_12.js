b4c817bc841961fb27467a7476cc06af57620c82
Vincent La (via Travis CI) <vincela9@gmail.com>
2020-05-04 00:50:30 +0000

    Deploy vincentlaucsb/csv-parser to github.com/vincentlaucsb/csv-parser.git:gh-pages

--- html/search/all_12.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['variable_5fcolumns',['variable_columns',['../classcsv_1_1CSVFormat.html#a00d9f6ea821cb0570c3088cebf5e2f62',1,'csv::CSVFormat::variable_columns(VariableColumnPolicy policy=VariableColumnPolicy::IGNORE)'],['../classcsv_1_1CSVFormat.html#a3434d223edcc2753851e448b145fe944',1,'csv::CSVFormat::variable_columns(bool policy)']]],
-  ['variablecolumnpolicy',['VariableColumnPolicy',['../namespacecsv.html#a7ba729051df241fedb28419ae2ba631a',1,'csv']]]
-];
--- html/search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['if_5fconstexpr',['IF_CONSTEXPR',['../compatibility_8hpp.html#a47ec10d8c001b6c6d05b8c88c9dbe554',1,'compatibility.hpp']]]
-];
