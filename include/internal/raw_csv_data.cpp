90817890b61aab12e63882b5ff97250e59b45a63
Vincent La <vincela9@gmail.com>
2020-09-12 14:54:04 -0700

    Fixed issue with quoted fields

--- include/internal/raw_csv_data.cpp
+++ include/internal/raw_csv_data.cpp
@@ -21,12 +21,18 @@ namespace csv {
         for (size_t i = 0; i < in.size(); ) {
             switch (parse_flags[in[i] + 128]) {
             case ParseFlags::NOT_SPECIAL:
-                this->raw_data->fields.push_back(
-                    this->parse_field(in, parse_flags, i)
-                );
-                break;
+                // TODO: Simplify this?
+                if (!quote_escape) {
+                    this->raw_data->fields.push_back(
+                        this->parse_field(in, parse_flags, i)
+                    );
+                    break;
+                }
+
+                HEDLEY_FALL_THROUGH;
 
             case ParseFlags::QUOTE:
+                i++;
                 quote_escape = true;
                 this->raw_data->fields.push_back(
                     this->parse_quoted_field(in, parse_flags, i, quote_escape)
@@ -75,7 +81,7 @@ namespace csv {
                     if (next_ch >= ParseFlags::DELIMITER) {
                         // Case: Delim or newline => end of field
                         quote_escape = false;
-
+                        i++;
                         return {
                             start,
                             length,
@@ -85,7 +91,7 @@ namespace csv {
                     else if (next_ch == ParseFlags::QUOTE) {
                         // Case: Escaped quote
                         has_double_quote = true;
-                        ++i;
+                        i++;
                     }
 
                     // Fallthrough
--- tests/test_raw_csv_data.cpp
+++ tests/test_raw_csv_data.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Basic CSV Parse Test", "[raw_csv_parse]") {
     std::string csv = "A,B,C\r\n" // Header row
         "123,234,345\r\n"
         "1,2,3\r\n"
-        "1,2,3";
+        "1,2,3\r\n";
 
     BasicCSVParser parser(std::move(csv));
     std::deque<RawCSVRow> rows;
@@ -22,4 +22,67 @@ TEST_CASE("Basic CSV Parse Test", "[raw_csv_parse]") {
 
     auto row = rows.front();
     REQUIRE(row.get_field(0) == "A");
+    REQUIRE(row.get_field(1) == "B");
+    REQUIRE(row.get_field(2) == "C");
+    REQUIRE(row.row_length == 3);
+
+    rows.pop_front();
+    row = rows.front();
+    REQUIRE(row.get_field(0) == "123");
+    REQUIRE(row.get_field(1) == "234");
+    REQUIRE(row.get_field(2) == "345");
+    REQUIRE(row.row_length == 3);
+
+    rows.pop_front();
+    row = rows.front();
+    REQUIRE(row.get_field(0) == "1");
+    REQUIRE(row.get_field(1) == "2");
+    REQUIRE(row.get_field(2) == "3");
+    REQUIRE(row.row_length == 3);
+
+    rows.pop_front();
+    row = rows.front();
+    REQUIRE(row.get_field(0) == "1");
+    REQUIRE(row.get_field(1) == "2");
+    REQUIRE(row.get_field(2) == "3");
+    REQUIRE(row.row_length == 3);
+}
+
+TEST_CASE("Test Quote Escapes", "[test_parse_quote_escape]") {
+    std::string csv = ""
+        "\"A\",\"B\",\"C\"\r\n"   // Quoted fields w/ no escapes
+        "123,\"234,345\",456\r\n" // Escaped comma
+        "1,\"2\"\"3\",4\r\n"      // Escaped quote
+        "1,2,3\r\n";
+
+    BasicCSVParser parser(std::move(csv));
+    std::deque<RawCSVRow> rows;
+    bool quote_escape = false;
+
+    parser.parse(
+        internals::make_parse_flags(',', '"'),
+        internals::WhitespaceMap(),
+        quote_escape,
+        rows
+    );
+
+    auto row = rows.front();
+    REQUIRE(row.get_field(0) == "A");
+    REQUIRE(row.get_field(1) == "B");
+    REQUIRE(row.get_field(2) == "C");
+    REQUIRE(row.row_length == 3);
+
+    rows.pop_front();
+    row = rows.front();
+    REQUIRE(row.get_field(0) == "123");
+    REQUIRE(row.get_field(1) == "234,345");
+    REQUIRE(row.get_field(2) == "456");
+    REQUIRE(row.row_length == 3);
+
+    rows.pop_front();
+    row = rows.front();
+    REQUIRE(row.get_field(0) == "1");
+    REQUIRE(row.get_field(1) == "2\"3");
+    REQUIRE(row.get_field(2) == "4");
+    REQUIRE(row.row_length == 3);
 }
